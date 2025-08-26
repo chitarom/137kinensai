944072d481c1d45b47244248352657fc65468655
智大 <anpanman0828@outlook.jp>
2025-08-26 16:11:24 +0900

    FooterNavコンポーネントのリファクタリングとスタイルの改善

--- my-app/src/FooterNav.jsx
+++ my-app/src/FooterNav.jsx
@@ -6,11 +6,44 @@ import home from './pictures/homeFooter.png'
 import event from './pictures/exclamationFooter.png'
 import map from './pictures/mapFooter.png'
 import schedule from './pictures/scheduleFooter.png'
+import { useLocation } from 'react-router-dom';
 
 function FooterNav() {
+    const iconMap = {
+        home,
+        search,
+        map,
+        schedule,
+        event
+    }
+    const location = useLocation()
+    const currentPath = location.pathname
+
+    const navItems = [
+        { label: 'ホーム', name:'home', path: '/' },
+        { label: '企画を探す', name:'search', path: '/search' },
+        { label: 'マップ', name:'map', path: '/map' },
+        { label: '予定表', name:'schedule', path: '/schedule' },
+        { label: 'イベント', name:'event', path: '/event' }
+    ]
+
+    const enhancedNavItems = navItems.map(item => ({
+        ...item,
+        isActive: item.path === currentPath
+    }))
+
+
     return (
         <nav className="footer-nav">
-            <Link to="/" className="footer-button-con">
+            {enhancedNavItems.map(item => (
+                <Link key={item.path} to={item.path} className={`footer-button-con ${item.isActive ? 'isactive' : ''}`}>
+                    <img className="footer-button" src={iconMap[item.name]} alt={item.name} />
+                    <p>{item.label}</p>
+                </Link>
+            ))}
+
+
+{/* <Link to="/" className="footer-button-con">
                 <img className="footer-button" src={home} alt="home" />
                 <p>ホーム</p>
             </Link>
@@ -29,10 +62,10 @@ function FooterNav() {
             <Link to="/event" className="footer-button-con">
                 <img className="footer-button" src={event} alt="event" />
                 <p>イベント</p>
-            </Link>
+            </Link> */}
 
 
-        </nav>
+        </nav >
     )
 }
 
