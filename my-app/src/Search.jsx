b97d93c9aff8868e48a03af14cb5837d82057796
Eito64-X <151337882+Eito64-X@users.noreply.github.com>
2025-09-06 20:34:22 +0900

    Searchの開発2

--- my-app/src/Search.jsx
+++ my-app/src/Search.jsx
@@ -1,13 +1,22 @@
 import './Search.css'
-import TicketList from './SearchComponents/TicketList.jsx'; './TicketList.jsx';
+import data from './JSON/ProjectData.json'
+import TicketList from './SearchComponents/TicketList.jsx';import { useState } from 'react';
+ './TicketList.jsx';
 
 function Search() {
     //FavoriteかどうかはlocalStorageに保存
+    const [keyword,setKeyword] = useState("");
+    const [filter,setFilter] = useState([[],10]);
+    const search = (e) => {setKeyword(e.target.value);}
+    const changeFilterAll = () => {setFilter([[],10]);}
+    const changeFilterClass = () => {setFilter([[],11]);}
+    const changeFilterStage = () => {setFilter([[],7]);}
+    const changeFilterKodo = () => {setFilter([[],8]);}
+    const changeFilterYusi = () => {setFilter([[],9]);}
     return (
         <div>
             <div className='search-wrap'>
-                <input className='search-box2' type="text" id='keyword' />
-                <button className='search'>検索</button>
+                <input className='search-box2' type="text" id='keyword' onChange={search} placeholder='キーワードを入力...' />
             </div>
             <div className='filter-wrap'>
                 <div className='filter-list'>
@@ -18,8 +27,17 @@ function Search() {
                     <button className='filter inactive' id='4'>フィルタ4</button>
                 </div>
             </div>
-            <div className='tickets'>
-                <TicketList />
+            <div className='content'>
+                <div className='tabs'>
+                    <button className={"tab" + (filter[1] == 10 ? " active" : "")} onClick={changeFilterAll}>すべて</button>
+                    <button className={"tab" + (filter[1] == 11 ? " active" : "")} onClick={changeFilterClass}>クラス企画</button>
+                    <button className={"tab" + (filter[1] == 7 ? " active" : "")} onClick={changeFilterStage}>ステージ</button>
+                    <button className={"tab" + (filter[1] == 8 ? " active" : "")} onClick={changeFilterKodo}>講堂</button>
+                    <button className={"tab" + (filter[1] == 9 ? " active" : "")} onClick={changeFilterYusi}>有志</button>
+                </div>
+                <div className='tickets'>
+                    <TicketList keyword={keyword} filter={filter} />
+                </div>
             </div>
         </div>
     );
