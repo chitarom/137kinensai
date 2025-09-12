9d842362c4a3d570a8549497c23e0c245b144bc6
Chihiro <anpanman0828@outlook.jp>
2025-09-13 01:24:34 +0900

    マップ写らない問題修正の試み

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     host: '192.168.1.19', // ← ここがポイント
