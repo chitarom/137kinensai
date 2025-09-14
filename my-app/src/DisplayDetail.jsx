23a2e97786f50ec300e920f1a7afac1ab5553fb2
mikazuki2025 <038kazuki@gmail.com>
2025-09-14 17:31:20 +0900

    コメント機能追加

--- my-app/src/DisplayDetail.jsx
+++ my-app/src/DisplayDetail.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import { useRef, useState, useEffect } from 'react';
 import DetailImageSlider from './SearchComponents/DetailImageSlider';
+import { supabase } from './supabase';
 
 const DisplayDetail = ({ displayDetailContents, setDisplayingDetail }) => {
 
     const [favoriteSwitch, setFavoriteSwitch] = useState(false);
+    const [CommentList, setCommentList] = useState([]);
+    const [CurrentCommmentList, setCurrentCommentList] = useState([]);
 
     const hideDDetail = () => {
         setDisplayingDetail(-1);
@@ -33,6 +36,33 @@ const DisplayDetail = ({ displayDetailContents, setDisplayingDetail }) => {
         setFavoriteSwitch(!favoriteSwitch);
     }
 
+    // コメントに関して
+    useEffect(() => {
+        const fetchComments = async () => {
+            const { data, error } = await supabase
+                .from("comment")
+                .select("*")
+                .order("created_at", { ascending: true }) // 最新のものは後ろに来る
+                .eq("check", true); // 検閲済み
+            if (error) {
+                console.error("取得エラー:", error);
+            } else {
+                setCommentList(data);
+            }
+        }
+        fetchComments();
+    }, []);
+
+    useEffect(() => {
+        if (CommentList.length <= 0) return;
+        const data = CommentList.filter(
+            (item) => item.name === displayDetailContents[1]
+        );
+        setCurrentCommentList(data);
+        // console.log("Filtered");
+        // console.log(data);
+    }, [CommentList, displayDetailContents]);
+
     return (
         <div className='detail-wrap'>
             <div className='dw-margin' onClick={hideDDetail}></div>
@@ -51,6 +81,17 @@ const DisplayDetail = ({ displayDetailContents, setDisplayingDetail }) => {
                 </div>
                 <DetailImageSlider imagelist={displayDetailContents[4]} />
                 <div className='detail'>{displayDetailContents[2]}</div>
+                <div className='detail-comments'>
+                    {CurrentCommmentList.length > 0 ? (
+                        CurrentCommmentList.map((comment) => (
+                            <div key={comment.id} className='comment-item'>
+                                {comment.passage}
+                            </div>
+                        ))
+                    ) : (
+                        <p className='no-comment'>まだコメントはありません</p>
+                    )}
+                </div>
             </div>
         </div>
     )
