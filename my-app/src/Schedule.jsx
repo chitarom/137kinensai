19fd734b4a7cdc54baf618efc417fcf34e417df5
chitarom <anpanman0828@outlook.jp>
2025-09-19 16:47:01 +0900

    Update Schedule.jsx

--- my-app/src/Schedule.jsx
+++ my-app/src/Schedule.jsx
@@ -131,7 +131,7 @@ function Schedule() {
             {(displayingDetail >= 0) &&
                 (<DisplayDetail displayDetailContents={displayDetailContents} setDisplayingDetail={setDisplayingDetail} scheduled={true} />)
             }
-            <button className="page-changer" onClick={() => { setCurrentPage(prev => (prev + 1) % 2); setActiveTab("kodo"); }} >{currentPage === 0 ? '一覧表示' : 'タイムテーブル'}</button>
+{/*             <button className="page-changer" onClick={() => { setCurrentPage(prev => (prev + 1) % 2); setActiveTab("kodo"); }} >{currentPage === 0 ? '一覧表示' : 'タイムテーブル'}</button> */}
             {currentPage == 0 && <>
                 <div className="ko-or-st-con">
                     <div className="kodo-or-stage">
