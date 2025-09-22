b2c4bccd00ccc3c1d68137759a2d1f39c0b40fdb
太郎 <anpanman0828@outlook.jp>
2025-09-22 19:33:30 +0900

    整理券の配布について

--- my-app/src/AboutSeiriken.jsx
+++ my-app/src/AboutSeiriken.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 function AboutSeiriken() {
     return (
         <div className='about-seiriken-con'>
-            <h2>整理券配布について</h2>
-            <p>　記念祭をより快適にお楽しみ頂くため、一部のクラス企画では整理券制度を導入しております。</p>
+            <h1>整理券配布について</h1>
+            <img src="/pictures/整理券について.webp" className='seiriken-image'/>
+            <p>　整理券の配布時間は<span className='bold'>α・βの2パターン</span>があり、土曜日の場合、上図のように2時間周期で配布が行われます。</p>
+            <p>　なお、土曜・日曜ともに学校開場直後の公演のみ先着順となります。</p>
+            <p>　クラス前で待機することが可能になるのは、整理券に記載されている上演開始時刻の<span className='underline bold'>10分前</span>からです。</p>
             <br/>
-            <img src="/pictures/整理券example.png" className='seiriken-image'/>
-            <p>　整理券の配布時間はα・βの2 パターンが存在し、2時間周期で配布が行われます。配布開始から二時間先の開始分までの整理券が配布されます。配布時刻の詳細と各クラスがどの配布パターンに該当するかは下図をご覧ください。</p>
-            <p>なお、土曜・日曜ともに学校開場直後の公演のみ先着順となります。整理券配布は初回公演の入場が終了してから行われます。</p>
-            <p>クラス前で待機することが可能になるのは、整理券に記載されている上演開始時刻の<span className='underline bold'>10分前</span>からです。</p>
-            <Link className="button" to="/search">クラス企画の詳細はこちら</Link>
+            <p>　それぞれのクラス企画がどの整理券パターンかは、企画検索から確認いただけます。</p>
+            <Link className="button" to="/search">企画の検索はこちら</Link>
         </div>
     );
 };
