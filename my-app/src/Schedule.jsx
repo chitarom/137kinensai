823ce0ddcb7e28c50b43e8b9670ea46d16d723aa
mikazuki2025 <038kazuki@gmail.com>
2025-09-26 15:25:40 +0900

    時刻表示

--- my-app/src/Schedule.jsx
+++ my-app/src/Schedule.jsx
@@ -338,9 +338,11 @@ function Schedule() {
             {
                 <button className="page-changer" onClick={() => { setCurrentPage(prev => (prev + 1) % 2); setActiveTab("kodo"); }} >{currentPage === 0 ? '一覧表示' : 'タイムテーブル'}</button>
             }
-            {/*
-                <p className="current-time-fixed-display">現在<br/>{"12:34"}</p>
-            */}
+            {currentPage == 0 && (
+                <p className="current-time-fixed-display">現在<br/>
+                {`${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}`}</p>
+            )}
+            
             {currentPage == 0 && <>
                 <div className="ko-or-st-con">
                     <div className="kodo-or-stage">
